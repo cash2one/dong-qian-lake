fcac5b91064da85744c5850950902f56fa52e57c
caoguangyao <cgy8888@gmail.com>
2013-09-21 17:58:12 +0800

    add ie edge support

--- dong_qian_lake/dong_qian_lake/settings.py
+++ dong_qian_lake/dong_qian_lake/settings.py
@@ -1,5 +1,7 @@
 # Django settings for dong_qian_lake project.
 import os
+import dj_database_url
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,16 +12,18 @@ ADMINS = (
 APP_ROOT = os.path.dirname(__file__)
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(APP_ROOT,'data.sqlite3'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': os.path.join(APP_ROOT,'data.sqlite3'),                      # Or path to database file if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#    }
+#}
+
+DATABASES['default'] =  dj_database_url.config()
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
