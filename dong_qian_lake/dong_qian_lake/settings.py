eadd0a62e9caa66475aac350183c57a692c375db
guangyao <cgy8888@gmail.com>
2013-10-14 21:47:02 +0800

    change urls

--- dong_qian_lake/dong_qian_lake/settings.py
+++ dong_qian_lake/dong_qian_lake/settings.py
@@ -118,8 +118,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(APP_ROOT,'../mysite/templates'),
-    os.path.join(APP_ROOT,'../src/v1/templates'),
+#    os.path.join(APP_ROOT,'../mysite/templates'),
+#    os.path.join(APP_ROOT,'../src/v1/templates'),
     os.path.join(APP_ROOT,'../src/v2/templates'),
 )
 
@@ -132,7 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
-     'mysite',
+#     'mysite',
      'src.v2',
      'storages',
      'south',
