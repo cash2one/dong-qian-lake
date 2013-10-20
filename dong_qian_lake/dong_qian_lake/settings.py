3f15ed55dadb7a9b882e9c076ed0791730163e49
guangyao <cgy8888@gmail.com>
2013-10-20 13:48:34 +0800

    add static files

--- dong_qian_lake/dong_qian_lake/settings.py
+++ dong_qian_lake/dong_qian_lake/settings.py
@@ -76,7 +76,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(APP_ROOT,'../mysite/static'),
+#    os.path.join(APP_ROOT,'../mysite/static'),
     os.path.join(APP_ROOT,'../src/v1/static'),
     os.path.join(APP_ROOT,'../src/v2/static'),
 )
